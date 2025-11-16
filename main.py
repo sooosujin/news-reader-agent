9f61849e9d3c4ac0e0f229f2cc3f20b1fbe1ada4
sujin kim <sjvenus94@gmail.com>
2025-11-17 06:40:31 +0900

    Initial project setup and Add Translator feature

---
+++ main.py
@@ -0,0 +1,41 @@
+import dotenv
+
+dotenv.load_dotenv()
+
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
+
+@CrewBase
+class TranslatorCrew:
+    
+  
+  @agent
+  def translator_agent(self):
+    return Agent(
+      config= self.agents_config["translator_agent"],
+    )
+  
+  @task
+  def translate_task(self):
+    return Task(
+      config=self.tasks_config["translate_task"],
+        )
+  
+  @task
+  def naturalize_translation_task(self):
+    return Task(
+      config=self.tasks_config["naturalize_translation_task"],
+    )
+  
+  @crew
+  def assemble_crew(self):
+    return Crew(
+      agents=self.agents,
+      tasks=self.tasks,
+      verbose=True,
+    )
+    
+TranslatorCrew().assemble_crew().kickoff(
+  inputs={
+    "sentence": "The film captures the struggles of young adults trying to find their place in a rapidly changing world.",
+}) 
